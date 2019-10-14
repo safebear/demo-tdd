a7b35993d6dc05931e75ffe92dfb66cdee47ba49
Simon Stratton <simon.stratton@safebear.co.uk>
2019-10-14 13:06:14 +0100

    Read in HTML to our JQuery test

--- tests/display-result.test.js
+++ tests/display-result.test.js
@@ -2,25 +2,27 @@
  * @jest-environment jsdom
  */
 
-test('displays result after clicking on button', () => {
+const html = require('fs').readFileSync('./src/index.html').toString();
+const $ = require('jquery');
+
+jest
+  .dontMock('fs')
+  .dontMock('jquery');
+
+test('displays result after clicking on button', async () => {
+
 
   // Set up our document body
-  document.body.innerHTML =
-    '<input type="text" id="userInput" class="form-control" placeholder="Number of Sharks" />' +
-    '<button class="btn btn-primary btn-block" value="Submit" />' +
-    '<h3 class="text-center">Result</h3>' +
-    '<h1 class="text-center" id="result"><h1>';
+  document.documentElement.innerHTML = html.toString() 
 
   // This module has a side-effect
   require('../src/display-result');
-
-  const $ = require('jquery');
 
   // Use jquery to update our user input value
   $('#userInput').val("3");
 
   // Use jquery to emulate a click on our button
-  $('button.btn.btn-primary.btn-block').click();
+  $('.btn.btn-primary.btn-block').click();
 
   // Assert that the correct result is on screen
   expect($('h1#result').text()).toEqual('Jesus Wins');
